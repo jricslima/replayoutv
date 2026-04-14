084afcffa612d0d4d1f8bab7e91892624a5e0f77
jricslima <jricslima2@gmail.com>
2026-04-14 10:11:21 -0300

    Atualização Total: 14/04/2026 10:11

--- links_acervo.js
+++ links_acervo.js
@@ -31696,14 +31696,6 @@ filmes = [
         "n": 1
     },
     {
-        "t": "AMOR AS ESCURAS",
-        "c": "https://image.tmdb.org/t/p/w500/b1FOZOfVWnUGOybxO016ODaRbVt.jpg",
-        "a": "2014",
-        "link": "https://drive.google.com/file/d/1qait2K9eq6E9lz6P8Pdy74at8200sDwB/view?usp=sharing",
-        "tags": "DUB",
-        "n": 0
-    },
-    {
         "t": "CASANDO COM SR. DARCY",
         "c": "https://image.tmdb.org/t/p/w500/1pOXjaiBKbkEaHGOQ9Ss2MeaxrY.jpg",
         "a": "2018",

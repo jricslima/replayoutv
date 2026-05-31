effaff2a0b1d17bdb6068d9917a610381aec47aa
jricslima <jricslima2@gmail.com>
2026-05-31 16:19:30 -0300

    Atualização Total: 31/05/2026 16:19

--- links_acervo.js
+++ links_acervo.js
@@ -31904,7 +31904,7 @@ filmes = [
         "n": 0
     },
     {
-        "t": "PREMONIÇÃO (",
+        "t": "PREMONIÇÃO",
         "c": "https://image.tmdb.org/t/p/w300/yFJVHW2Q7ofpS0IJFPKBoonAbMe.jpg",
         "a": "2000",
         "link": "https://drive.google.com/file/d/17I5bOKX87gjxvcoS-4JmrbplsF7MW1cb/view?usp=sharing",
@@ -31912,7 +31912,7 @@ filmes = [
         "n": 0
     },
     {
-        "t": "PREMONIÇÃO 2 (",
+        "t": "PREMONIÇÃO 2",
         "c": "https://image.tmdb.org/t/p/w300/v1HSfT3BjDRGfHji30VvHeCBJHu.jpg",
         "a": "2003",
         "link": "https://drive.google.com/file/d/1uIUFxmMdvV6nuzQP2pNgVQPFFjnhxyzu/view?usp=sharing",
@@ -31920,7 +31920,7 @@ filmes = [
         "n": 0
     },
     {
-        "t": "PREMONIÇÃO 3 (",
+        "t": "PREMONIÇÃO 3",
         "c": "https://image.tmdb.org/t/p/w300/pIEOtja7W6BjFUiuvPI9kd3z3QT.jpg",
         "a": "2006",
         "link": "https://drive.google.com/file/d/1_362Wsnm8z6PXrBQLXdFism5_tD5BPuC/view?usp=sharing",
@@ -31928,7 +31928,7 @@ filmes = [
         "n": 0
     },
     {
-        "t": "PREMONIÇÃO 4 (",
+        "t": "PREMONIÇÃO 4",
         "c": "https://image.tmdb.org/t/p/w300/sIDOFaipvpSM0R2uZNcDMLvTbhM.jpg",
         "a": "2009",
         "link": "https://drive.google.com/file/d/1VZvNMBamLAMVeQGjNQ5XjjmC2j7E01Jp/view?usp=sharing",
@@ -31936,7 +31936,7 @@ filmes = [
         "n": 0
     },
     {
-        "t": "PREMONIÇÃO 5 (",
+        "t": "PREMONIÇÃO 5",
         "c": "https://image.tmdb.org/t/p/w300/A0C3juaReSw8vQD3aWEZnOgolFL.jpg",
         "a": "2011",
         "link": "https://drive.google.com/file/d/1BYUK-5PNmwIsP4mjeG5lKz7MF7WYUb0t/view?usp=sharing",
@@ -31944,7 +31944,7 @@ filmes = [
         "n": 0
     },
     {
-        "t": "PREMONIÇÃO 6 LAÇOS DE SANGUE (",
+        "t": "PREMONIÇÃO 6 - LAÇOS DE SANGUE",
         "c": "https://image.tmdb.org/t/p/w300/x3J781PsdMrjenzQKM5eJXqK5Nd.jpg",
         "a": "2025",
         "link": "https://drive.google.com/file/d/1yi9xMQkzZQKj9QxqOeK1mmD16T3f0g5s/view?usp=sharing",

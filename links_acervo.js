fd5272effe045c09ad7aa730b977aba7872b21af
jricslima <jricslima2@gmail.com>
2026-05-31 09:55:31 -0300

    Atualização Total: 31/05/2026 09:55

--- links_acervo.js
+++ links_acervo.js
@@ -22856,9 +22856,9 @@ filmes = [
         "n": 0
     },
     {
-        "t": "PANDA! GO PANDA! MP4 / MKV",
-        "c": "https://placehold.jp/24/111111/ffffff/300x450.png?text=Sem%20Capa",
-        "a": "----",
+        "t": "PANDA! GO PANDA! AS AVENTURAS DE PANDA E SEUS AMIGOS",
+        "c": "https://www.themoviedb.org/t/p/w600_and_h900_face/sqGZ7JOSDDM6WppwkE8PIPgrlAo.jpg",
+        "a": "2017",
         "link": "https://drive.google.com/file/d/1hsjU9N_vPCRgkP3prXbL3MJm231lcPMQ/view?usp=sharing",
         "tags": "DUB",
         "n": 0

5033bbdb9f740e4860411db445cbd8779f8a4d28
jricslima <jricslima2@gmail.com>
2026-06-01 13:18:42 -0300

    Atualização Total: 01/06/2026 13:18

--- links_acervo.js
+++ links_acervo.js
@@ -24968,9 +24968,9 @@ filmes = [
         "n": 0
     },
     {
-        "t": "ROYALLY WRAPPED FOR CHRISTMAS MP4 (LUCIANA)",
-        "c": "https://placehold.jp/24/111111/ffffff/300x450.png?text=Sem%20Capa",
-        "a": "----",
+        "t": "ROYALLY WRAPPED FOR CHRISTMAS",
+        "c": "https://www.themoviedb.org/t/p/w600_and_h900_face/1X0C6fBt1tWR9lpwdrwjVqhWpOV.jpg",
+        "a": "2021",
         "link": "https://drive.google.com/file/d/1VaY846dGIOSL6uF1KJJ-n5EHSrGn6qGU/view?usp=sharing",
         "tags": "LEG",
         "n": 0

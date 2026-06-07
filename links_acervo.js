928822eb87b4013d4983ae41c334178298027c0c
jricslima <jricslima2@gmail.com>
2026-06-07 19:01:46 -0300

    Atualização Total: 07/06/2026 19:01

--- links_acervo.js
+++ links_acervo.js
@@ -26752,9 +26752,9 @@ filmes = [
         "n": 0
     },
     {
-        "t": "TÁTICAS DO AMOR DUBLADO",
-        "c": "https://placehold.jp/24/111111/ffffff/300x450.png?text=Sem%20Capa",
-        "a": "----",
+        "t": "TÁTICAS DO AMOR",
+        "c": "https://www.themoviedb.org/t/p/w600_and_h900_face/96EnxT3dsE1Aob6CZOM5WWCt8bI.jpg",
+        "a": "2022",
         "link": "https://drive.google.com/file/d/12bmiShMKNWtK9QQIef5iuEScEcJQRp_y/view?usp=sharing",
         "tags": "DUB",
         "n": 0

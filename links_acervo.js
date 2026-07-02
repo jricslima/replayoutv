85ce1f95be93812e34f914f83bdc24d53069e9a1
jricslima <jricslima2@gmail.com>
2026-07-02 12:12:22 -0300

    Atualização Total: 02/07/2026 12:12

--- links_acervo.js
+++ links_acervo.js
@@ -30056,9 +30056,9 @@ filmes = [
         "n": 0
     },
     {
-        "t": "UMA QUEDINHA DE NATAL MP4",
-        "c": "https://placehold.jp/24/111111/ffffff/300x450.png?text=Sem%20Capa",
-        "a": "----",
+        "t": "UMA QUEDINHA DE NATAL",
+        "c": "https://www.themoviedb.org/t/p/w600_and_h900_face/p9wwj23RF3Nqjj0EzxsDx0s2aV7.jpg",
+        "a": "2022",
         "link": "https://drive.google.com/file/d/1uZoyNkz88FQ1EEtwzfWRXiUo4zoL_eQW/view?usp=sharing",
         "tags": "DUB",
         "n": 1

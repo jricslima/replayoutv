d35bc657a8bff83b2b03f0838938a8492a95b277
jricslima <jricslima2@gmail.com>
2026-07-02 11:45:24 -0300

    Atualização Total: 02/07/2026 11:45

--- links_acervo.js
+++ links_acervo.js
@@ -29096,9 +29096,9 @@ filmes = [
         "n": 0
     },
     {
-        "t": "UM DIA PARA SEMPRE MKV",
-        "c": "https://placehold.jp/24/111111/ffffff/300x450.png?text=Sem%20Capa",
-        "a": "----",
+        "t": "UM DIA PARA SEMPRE",
+        "c": "https://www.themoviedb.org/t/p/w600_and_h900_face/77fwdB0FqWWOvNjEaOwIqMW58DM.jpg",
+        "a": "2020",
         "link": "https://drive.google.com/file/d/1E0fB6SP1yhsSeRmW1CnCp8bI69x6lPv7/view?usp=sharing",
         "tags": "DUB",
         "n": 0

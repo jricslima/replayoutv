89159bee496a706f0c4644c33236011c00c96858
jricslima <jricslima2@gmail.com>
2026-07-19 11:46:27 -0300

    Atualização Total: 19/07/2026 11:46

--- links_acervo.js
+++ links_acervo.js
@@ -32262,5 +32262,13 @@ filmes = [
         "link": "https://drive.google.com/file/d/1_iga3LSstAlpsWKXOXgzTirEbUxn1J-Y/view?usp=sharing",
         "tags": "DUAL",
         "n": 0
+    },
+    {
+        "t": "VERÃO A SUMMER ROMANCE",
+        "c": "https://image.tmdb.org/t/p/w300/dLToNkE52Fjig78MWisKmUOniG1.jpg",
+        "a": "2019",
+        "link": "https://drive.google.com/file/d/1Tti9GqgbN5LsAnrouNOGq4672wCiiFSg/view?usp=sharing",
+        "tags": "DUB",
+        "n": 0
     }
 ];

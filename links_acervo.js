ec5bd2327fd070d6d01355eed369b7d979f9d2ae
jricslima <jricslima2@gmail.com>
2026-08-06 12:07:17 -0300

    Atualização Total: 06/08/2026 12:07

--- links_acervo.js
+++ links_acervo.js
@@ -9371,8 +9371,8 @@ filmes = [
         "t": "CONTATO DE RISCO",
         "c": "https://image.tmdb.org/t/p/w300/rZYBM8NJckYGaHreVR2ztJ3WJsD.jpg",
         "a": "2003",
-        "link": "https://drive.google.com/file/d/13swBbCiFEVtHsWAXlxtsfqiyGw08jbpW/view?usp=drive_open",
-        "tags": "LEG",
+        "link": "https://drive.google.com/file/d/1RxLP7iK5wyg2Ro_0wCV7yv_ClLtWUgzC/view?usp=sharing",
+        "tags": "LEG AC",
         "n": 0
     },
     {

ca48c237bc96146065230c424de682e27e628d51
jricslima <jricslima2@gmail.com>
2026-08-15 20:50:34 -0300

    Atualização Total: 15/08/2026 20:50

--- links_acervo.js
+++ links_acervo.js
@@ -31016,7 +31016,7 @@ filmes = [
         "n": 0
     },
     {
-        "t": "POLO NORTE",
+        "t": "POLO NORTE - NORTHPOLE",
         "c": "https://image.tmdb.org/t/p/w300/iJBQqFq3jDdHpckoephqIIMlPvf.jpg",
         "a": "2014",
         "link": "https://drive.google.com/file/d/1TYlzhnU-F_RmQojQO8lJstzlJr6quO2L/view",

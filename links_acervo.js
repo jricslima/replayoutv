c7f4ee82b641b0ea53dfa92eda24183ca63d9812
jricslima <jricslima2@gmail.com>
2026-09-06 21:16:30 -0300

    Atualização Total: 06/09/2026 21:16

--- links_acervo.js
+++ links_acervo.js
@@ -33086,5 +33086,13 @@ filmes = [
         "link": "https://u.pcloud.link/publink/show?code=XZu7cpJZekGTUOmjjzbTznbxo1FoWyM1kAHk",
         "tags": "LEG",
         "n": 0
+    },
+    {
+        "t": "THE CHRISTMAS CHALET O CHALÉ DE NATAL",
+        "c": "https://www.themoviedb.org/t/p/w600_and_h900_face/ftx4UMnWx06pLRn84qLMBCFfMnp.jpg",
+        "a": "2019",
+        "link": "https://u.pcloud.link/publink/show?code=XZsLyHJZAved7pa7MRh2wIA7PSaJUJKsbCXV",
+        "tags": "DUB",
+        "n": 1
     }
 ];

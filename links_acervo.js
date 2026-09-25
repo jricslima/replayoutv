8eb3b5fce891247ec3434e9a7550b60fc44e2f6b
jricslima <jricslima2@gmail.com>
2026-09-25 19:49:19 -0300

    Atualização Total: 25/09/2026 19:49

--- links_acervo.js
+++ links_acervo.js
@@ -33150,5 +33150,13 @@ filmes = [
         "link": "https://u.pcloud.link/publink/show?code=XZhtf4JZVtplOLeRNRzAM4y8zUG127k8cRNX",
         "tags": "LEG",
         "n": 0
+    },
+    {
+        "t": "A CANOA FUROU DONT GIVE UP THE SHIP",
+        "c": "https://image.tmdb.org/t/p/w300/dstIXqOUbNK2fpIuPZBck8dmprO.jpg",
+        "a": "1959",
+        "link": "https://u.pcloud.link/publink/show?code=XZk5U4JZynPNVgHx5i4k0OGzvIPiYzBMQnCX",
+        "tags": "DUAL",
+        "n": 0
     }
 ];
